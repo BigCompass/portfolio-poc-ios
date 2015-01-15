ed4d37aa29efa4ccf2725755528503c6a1fc5879
Rob Pagano <rob@bigcompass.com>
2015-01-15 13:09:44 -0700

    login and register layout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,1 +1,9 @@
 angular.module('portfolio.controllers', [])
+
+.controller('AuthCtrl', function ($scope) {
+	$scope.message = 'auth controller';
+})
+
+.controller('PortfolioCtrl', function ($scope) {
+	$scope.message = 'portfolio controller';
+});
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,1 +1,41 @@
 angular.module('portfolio.routes', [])
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+
+			.state('start', {
+				url: '/',
+				templateUrl: 'templates/start.html',
+				controller: 'AuthCtrl'
+			})
+
+			.state('login', {
+				url: '/login',
+				templateUrl: 'templates/login.html',
+				controller: 'AuthCtrl'
+			})
+
+			.state('register', {
+				url: '/register',
+				templateUrl: 'templates/register.html',
+				controller: 'AuthCtrl'
+			})
+
+			.state('tab', {
+				url: '/tab',
+				abstract: true,
+				templateUrl: 'templates/tabs.html'
+			})
+
+			.state('tab.portfolio', {
+			  url: '/portfolios',
+			  views: {
+			    'tab-portfolio': {
+			      templateUrl: 'templates/tab-portfolio.html',
+			      controller: 'PortfolioCtrl'
+			    }
+			  }
+			});
+
+			// dash if nothing
+			$urlRouterProvider.otherwise('/');
+	});
