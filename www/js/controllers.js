26b70118d524da70dee8f677a39e756f46f86656
Rob Pagano <rob@bigcompass.com>
2015-01-15 13:24:04 -0700

    tab layout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,4 +6,8 @@ angular.module('portfolio.controllers', [])
 
 .controller('PortfolioCtrl', function ($scope) {
 	$scope.message = 'portfolio controller';
+})
+
+.controller('QuoteCtrl', function ($scope) {
+	$scope.message = 'quote controller';
 });
--- www/js/routes.js
+++ www/js/routes.js
@@ -26,6 +26,26 @@ angular.module('portfolio.routes', [])
 				templateUrl: 'templates/tabs.html'
 			})
 
+			.state('tab.quote', {
+			  url: '/quote',
+			  views: {
+			    'tab-quote': {
+			      templateUrl: 'templates/tab-quote.html',
+			      controller: 'QuoteCtrl'
+			    }
+			  }
+			})
+
+			.state('tab.account', {
+			  url: '/account',
+			  views: {
+			    'tab-account': {
+			      templateUrl: 'templates/tab-account.html',
+			      controller: 'AuthCtrl'
+			    }
+			  }
+			})
+
 			.state('tab.portfolio', {
 			  url: '/portfolios',
 			  views: {
