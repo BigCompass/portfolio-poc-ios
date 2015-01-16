327df76fd096d85af6f3d6dc12274cc610ca2801
Rob Pagano <rob@bigcompass.com>
2015-01-16 08:47:36 -0700

    tab styling

--- www/js/controllers/auth.js
+++ www/js/controllers/auth.js
@@ -1,13 +1,8 @@
 app.controller('AuthCtrl', function ($scope, $state, Auth, user) {
 
-	console.log('user before:');
-	console.log($scope.user);
-
 	if (user) {
 	  $state.go('tab.portfolio');
 	}
-
-	console.log($scope.user);
 
 	$scope.logUser = function () {
 		console.log($scope.user);
