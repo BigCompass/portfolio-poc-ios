9574a50d161d8323b539d471e2db4f01b21159bb
Rob Pagano <rob@bigcompass.com>
2015-01-20 10:28:58 -0700

    ticker symbol validation

--- www/js/controllers/quote.js
+++ www/js/controllers/quote.js
@@ -1,10 +1,40 @@
 app.controller('QuoteCtrl', function ($scope, $http) {
+	var LOOKUP_URL = 'http://dev.markitondemand.com/Api/v2/Lookup/jsonp';
 	var QUOTE_URL = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp';
 
-	$scope.investment = {};
+	$scope.investment = {
+		isValid: false,
+		lookup: {}
+	};
+
+
+	$scope.validateSymbol = function() {
+		// starts as false for each check
+		$scope.investment.isValid = false;
+		$scope.investment.lookup = {};
+
+		$http.jsonp(LOOKUP_URL, { params: { input: $scope.investment.symbol, callback: 'JSON_CALLBACK' } })
+			.success(function (data) {
+				var symbol = $scope.investment.symbol.toUpperCase();
+				
+				for(var i = 0; i < data.length && !$scope.investment.isValid; i++) {
+					
+					// match on exact symbol only
+					if(data[i].Symbol === symbol) {
+						$scope.investment.isValid = true;
+						// set lookup
+						$scope.investment.lookup = data[i];
+					}
+				}
+			})
+			.error(function (e) {
+				console.log(e);
+		});
+	};
 
 	$scope.getQuote = function() {
-		console.log($scope.investment.symbol);
+		// clear display area regardless
+		$scope.investment.quoteData = null;
 
 		$http.jsonp(QUOTE_URL, { params: { symbol: $scope.investment.symbol, callback: 'JSON_CALLBACK' } })
 			.success(function (data) {
